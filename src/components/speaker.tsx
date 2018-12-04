01e38e8548c1dd95bed35f463e71e8eadc3b0ca2
Timur Kiyui <timur@linux.com>
2018-12-05 00:21:26 +0800

    refactor(src): convert to rxjs 6

--- src/components/speaker.tsx
+++ src/components/speaker.tsx
@@ -1,8 +1,8 @@
-import xs, { Stream } from 'xstream';
-import sampleCombine from 'xstream/extra/sampleCombine';
-import { VNode, DOMSource, div, h2, textarea, button } from '@cycle/dom';
+import { of as observableOf, merge as observableMerge, Observable } from 'rxjs';
+import { map, mapTo, sample } from 'rxjs/operators';
+import { VNode, div, h2, textarea, button } from '@cycle/dom';
 
-import { Sources, Sinks, Reducer } from '../interfaces';
+import { Sources, Sinks, Reducer, DOMSource, StateSource } from '../interfaces';
 
 export interface State {
     text: string;
@@ -10,9 +10,9 @@ export interface State {
 export const defaultState: State = { text: 'Edit me!' };
 
 export interface DOMIntent {
-    speech$: Stream<null>;
-    link$: Stream<null>;
-    updateText$: Stream<string>;
+    speech$: Observable<null>;
+    link$: Observable<null>;
+    updateText$: Observable<string>;
 }
 
 export function Speaker({ DOM, state }: Sources<State>): Sinks<State> {
@@ -26,59 +26,70 @@ export function Speaker({ DOM, state }: Sources<State>): Sinks<State> {
     };
 }
 
-function model(updateText$: Stream<string>): Stream<Reducer<State>> {
-    const init$ = xs.of<Reducer<State>>(() => defaultState);
+function model(updateText$: Observable<string>): Observable<Reducer<State>> {
+    const init$ = observableOf<Reducer<State>>(() => defaultState);
 
-    const update$ = updateText$.map(text => (state: State) => ({
-        ...state,
-        text
-    }));
+    const update$ = updateText$.pipe(
+        map(text => (state: State) => ({
+            ...state,
+            text
+        }))
+    );
 
-    return xs.merge(init$, update$);
+    return observableMerge(init$, update$);
 }
 
-function view(state$: Stream<State>): Stream<VNode> {
-    return state$.map(({ text }) =>
-        div([
-            h2('My Awesome Cycle.js app - Page 2'),
-            textarea({
-                attrs: { id: 'text', rows: '3' },
-                props: { value: text }
-            }),
-            button(
-                { attrs: { type: 'button' }, dataset: { action: 'speak' } },
-                ['Speak to Me!']
-            ),
-            button(
-                { attrs: { type: 'button' }, dataset: { action: 'navigate' } },
-                ['Page 1']
-            )
-        ])
+function view(state$: Observable<State>): Observable<VNode> {
+    return state$.pipe(
+        map(({ text }) =>
+            div([
+                h2('My Awesome Cycle.js app - Page 2'),
+                textarea({
+                    attrs: { id: 'text', rows: '3' },
+                    props: { value: text }
+                }),
+                button(
+                    { attrs: { type: 'button' }, dataset: { action: 'speak' } },
+                    ['Speak to Me!']
+                ),
+                button(
+                    {
+                        attrs: { type: 'button' },
+                        dataset: { action: 'navigate' }
+                    },
+                    ['Page 1']
+                )
+            ])
+        )
     );
 }
 
 function intent(DOM: DOMSource): DOMIntent {
     const updateText$ = DOM.select('#text')
         .events('input')
-        .map((ev: any) => ev.target.value);
+        .pipe(map((ev: any) => ev.target.value));
 
     const speech$ = DOM.select('[data-action="speak"]')
         .events('click')
-        .mapTo(null);
+        .pipe(mapTo(null));
 
     const link$ = DOM.select('[data-action="navigate"]')
         .events('click')
-        .mapTo(null);
+        .pipe(mapTo(null));
 
     return { updateText$, speech$, link$ };
 }
 
-function redirect(link$: Stream<any>): Stream<string> {
-    return link$.mapTo('/counter');
+function redirect(link$: Observable<any>): Observable<string> {
+    return link$.pipe(mapTo('/counter'));
 }
 
-function speech(speech$: Stream<any>, state$: Stream<State>): Stream<string> {
-    return speech$
-        .compose(sampleCombine(state$))
-        .map(([_, s]: [any, State]) => s.text);
+function speech(
+    speech$: Observable<any>,
+    state$: Observable<State>
+): Observable<string> {
+    return speech$.pipe(
+        sample(state$),
+        map(([_, s]: [any, State]) => s.text)
+    );
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,17 @@
-import { Stream } from 'xstream';
-import { DOMSource, VNode } from '@cycle/dom';
-import { StateSource, Reducer } from '@cycle/state';
-import { RouterSource, HistoryAction } from 'cyclic-router';
+import { Observable } from 'rxjs';
+import { VNode } from '@cycle/dom';
+import { DOMSource } from '@cycle/dom/lib/cjs/rxjs';
+import { Reducer } from '@cycle/state';
+import { HistoryAction } from 'cyclic-router';
+import { RouterSource } from 'cyclic-router/rxjs-typings';
 
+// Exports
+export { DOMSource } from '@cycle/dom/lib/cjs/rxjs';
 export { Reducer } from '@cycle/state';
+// @cycle/state does not have an RXJS version
+export interface StateSource<S> {
+    stream: Observable<S>;
+}
 
 export type Component<State> = (s: Sources<State>) => Sinks<State>;
 
@@ -14,8 +22,8 @@ export interface Sources<State> {
 }
 
 export interface Sinks<State> {
-    DOM?: Stream<VNode>;
-    router?: Stream<HistoryAction>;
-    speech?: Stream<string>;
-    state?: Stream<Reducer<State>>;
+    DOM?: Observable<VNode>;
+    router?: Observable<HistoryAction>;
+    speech?: Observable<string>;
+    state?: Observable<Reducer<State>>;
 }
